14f2c59d420138773cf671b90aacdc070e54fff2
Emil Motrøen <e.motroen@gmail.com>
2024-05-12 17:59:40 +0200

    Add project files.

---
+++ FrostAPIService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace FrostAPIService
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+
+            var repositories = await ProcessFrostAsync(client);
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine($"Name: {repo.Name}");
+                Console.WriteLine($"Homepage: {repo.Homepage}");
+                Console.WriteLine($"GitHub: {repo.GitHubHomeUrl}");
+                Console.WriteLine($"Description: {repo.Description}");
+                Console.WriteLine($"Watchers: {repo.Watchers:#,0}");
+                Console.WriteLine($"Last push: {repo.LastPush}");
+                Console.WriteLine();
+            }
+        }
+
+        static async Task<List<Repository>> ProcessFrostAsync(HttpClient client)
+        {
+            await using Stream stream =
+                await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories =
+                await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+            return repositories ?? new();
+        }
+    }
+}
